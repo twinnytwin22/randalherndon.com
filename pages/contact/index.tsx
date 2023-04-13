cf12ab19cc7d75951c6cd64db32569fc41adc1b0
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-13 03:11:14 -0700

    lpotn

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,13 +1,14 @@
-require('dotenv').config()
 const sgMail = require('@sendgrid/mail')
-const {SENDGRID_API_KEY, FROM_EMAIL, TO_EMAIL} = process.env
-sgMail.setApiKey(SENDGRID_API_KEY)
+const from = process.env.FROM_EMAIL
+const to = process.env.TO_EMAIL
+const api =process.env.SENDGRID_API_KEY
+sgMail.setApiKey(api)
 
 export default async function (req:any, res: any) {
  const {subject, email, message}= req.body
     const msg = {
-      to: TO_EMAIL, // Change to your recipient
-      from: FROM_EMAIL, // Change to your verified sender
+      to: to, // Change to your recipient
+      from: from, // Change to your verified sender
       subject: 'The CRIB Contact Form',
       text: 'New Message from the CRIB',
       html: `<strong>${subject}</strong>,
