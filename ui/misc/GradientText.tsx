dbcb51144fa089d860f16b1e66b12ff71f562431
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-16 02:23:20 -0700

    Update GradientText.tsx

--- ui/misc/GradientText.tsx
+++ ui/misc/GradientText.tsx
@@ -4,7 +4,7 @@ import { m } from "framer-motion";
 export const GradientText = ({text}:any) => {
   const gradient = "linear-gradient(to right, #666699, #3366cc, #6699ff)";
   return (
-    <div className="">
+    <div className=" pl-6 text-left hidden md:block">
     <m.h1
       className=" bg-clip-text text-transparent text-4xl md:ext-6xl "
       style={{ backgroundImage: gradient, backgroundSize: "600% 100%" ,  animation: "slide 10s linear infinite",
