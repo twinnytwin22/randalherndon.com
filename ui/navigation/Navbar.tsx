d7710cac1fd4e94aab0cc94563b7125bbf0d9b95
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-14 15:22:15 -0700

    Update Navbar.tsx

--- ui/navigation/Navbar.tsx
+++ ui/navigation/Navbar.tsx
@@ -7,10 +7,10 @@ import { RHLogo } from "../misc/RHLogo";
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(true);
   const [showNavContent, setShowNavContent] = useState(false);
-  const [ isOpen, setIsOpen ] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
 
-  
+
 
   const handleShowContent = () => {
     setShowNavContent(!showNavContent);
@@ -19,76 +19,75 @@ const Navbar = () => {
   return (
     <AnimatePresence>
       <div className="content-center mx-auto justify-center opacity-90 absolute items-center">
-          <m.nav
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 0.8,
-              delay: 0.5,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-            className="fixed flex items-center justify-between top-3 right-0 left-0 w-[90vw] max-w-lg md:max-w-5xl mx-auto py-2.5 px-8 rounded-xl transition-all duration-300 dark:bg-black bg-slate-200 border-slate-300 dark:border-gray-900 shadow-slate-300 dark:shadow-gray-700 shadow-sm border opacity-80"
-          >
-            <RHLogo/>
-            <nav className="hidden md:flex w-full mx-auto px-4 items-center h-fit text-center justify-center font-[owners-wide]">
-          <div className="flex content-between mx-auto items-center h-fit w-full justify-center">
-            <div className="mx-4  hover:scale-105">
-              <a href="#about">About</a>
-            </div>
-            <div className="mx-4  hover:scale-105">
-              <a href="#skills">Experience</a>
-            </div>
-            <div className="mx-4 hover:scale-105">
-              <a href="#portfolio">Portfolio</a>
-            </div>
-         
-          </div>
-          <div className="flex justify-center mt-8">
-            
-          </div>
-        </nav>
-            <div className="flex items-center">
-              <div className="block lg:hidden mr-3">
-                <DarkModeSwitch />
+        <m.nav
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          className="fixed flex items-center justify-between top-3 right-0 left-0 w-[90vw] max-w-lg md:max-w-5xl mx-auto py-2.5 px-8 rounded-xl transition-all duration-300 dark:bg-black bg-slate-200 border-slate-300 dark:border-gray-900 shadow-slate-300 dark:shadow-gray-700 shadow-sm border opacity-80"
+        >
+          <RHLogo />
+          <nav className="hidden md:flex w-full mx-auto px-4 items-center h-fit text-center justify-center font-[owners-wide]">
+            <div className="flex content-between mx-auto items-center h-fit w-full justify-center font-bold">
+              <div className="mx-4  hover:scale-105">
+                <a href="#about">About</a>
               </div>
-              <div
-                onClick={handleShowContent}
-                className={`border-2  dark:border-slate-200 border-gray-900 rounded-full block md:hidden ${
-                  showNavContent && "rotate-90 ease-in-out duration-300"
+              <div className="mx-4  hover:scale-105">
+                <a href="#skills">Experience</a>
+              </div>
+              <div className="mx-4 hover:scale-105">
+                <a href="#portfolio">Portfolio</a>
+              </div>
+
+            </div>
+            <div className="flex justify-center mt-8">
+
+            </div>
+          </nav>
+          <div className="flex items-center">
+            <div className="block lg:hidden mr-3">
+              <DarkModeSwitch />
+            </div>
+            <div
+              onClick={handleShowContent}
+              className={`border-2  dark:border-slate-200 border-gray-900 rounded-full block md:hidden ${showNavContent && "rotate-90 ease-in-out duration-300"
                 } `}
+            >
+              <svg
+                className="w-8 p-1"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={3}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
               >
-                <svg
-                  className="w-8 p-1"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={3}
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
             </div>
-          </m.nav>
-    
+          </div>
+        </m.nav>
+
         {showNavContent && showNavbar && (
 
-      <NavContent showNavbar={showNavbar} setShowNavbar={setShowNavbar}/>)}
-    </div></AnimatePresence>
+          <NavContent showNavbar={showNavbar} setShowNavbar={setShowNavbar} />)}
+      </div></AnimatePresence>
   );
 };
 
 export default Navbar;
 
-const NavContent = ({setShowNavContent}:any) => {
+const NavContent = ({ setShowNavContent }: any) => {
   return (
     <AnimatePresence>
-      <m.div 
+      <m.div
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0 }}
@@ -118,7 +117,7 @@ const NavContent = ({setShowNavContent}:any) => {
             </div>
           </div>
           <div className="flex justify-center mt-8">
-            
+
           </div>
         </nav>
       </m.div>
@@ -126,5 +125,5 @@ const NavContent = ({setShowNavContent}:any) => {
   );
 };
 
-  
+
 
