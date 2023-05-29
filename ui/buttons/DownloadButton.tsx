66cb59564085cacd2cb8a4d7c1d6003f3b5ffb91
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-29 09:38:59 -0700

    update resume link

--- ui/buttons/DownloadButton.tsx
+++ ui/buttons/DownloadButton.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaFileDownload } from "react-icons/fa";
 export const DownloadButton = () => {
-    const [showTooltip, setShowTooltip] = useState(false);
-    return (
-      <Link href="/RHresume23.pdf" className="flex rounded-lg p-4 text-lg hover:scale-105 text-white bg-blue-700" 
+  const [showTooltip, setShowTooltip] = useState(false);
+  return (
+    <Link href="/Resume-Randal-Herndon.pdf" className="flex rounded-lg p-4 text-lg hover:scale-105 text-white bg-blue-700"
       onMouseOver={() => setShowTooltip(true)}
       onMouseOut={() => setShowTooltip(false)}>
-        <span
-         
-          className="relative inline-block"
-        >
-          <FaFileDownload />
-          {showTooltip && (
-            <span className="absolute top-full left-1/2 transform -translate-x-1/2 py-2 px-4 bg-black text-white text-xs rounded-md whitespace-nowrap">
-              Download my resume
-            </span>
-          )}
-        </span>
-      </Link>
-    );
-  };
+      <span
+
+        className="relative inline-block"
+      >
+        <FaFileDownload />
+        {showTooltip && (
+          <span className="absolute top-full left-1/2 transform -translate-x-1/2 py-2 px-4 bg-black text-white text-xs rounded-md whitespace-nowrap">
+            Download my resume
+          </span>
+        )}
+      </span>
+    </Link>
+  );
+};
