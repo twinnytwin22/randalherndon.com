1005f3840c25f47a8965d1d61d699d1c9d6c8186
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-13 21:54:33 -0700

    update

--- ui/buttons/DownloadButton.tsx
+++ ui/buttons/DownloadButton.tsx
@@ -5,7 +5,7 @@ import { FaFileDownload } from "react-icons/fa";
 export const DownloadButton = () => {
   const [showTooltip, setShowTooltip] = useState(false);
   return (
-    <Link href="/Resume-Randal-Herndon.pdf" className="flex rounded-lg p-4 text-lg hover:scale-105 duration-200 ease-in-out text-white bg-blue-700"
+    <Link href="/Randal-H-Resume.pdf" className="flex rounded-lg p-4 text-lg hover:scale-105 duration-200 ease-in-out text-white bg-blue-700"
       onMouseOver={() => setShowTooltip(true)}
       onMouseOut={() => setShowTooltip(false)}>
       <span
