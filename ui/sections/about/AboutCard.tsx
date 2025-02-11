55418e5c0e5778be1f99daa9b73daa14b376119c
Randal Herndon / Twinny Twin <randal@cribnetwork.io>
2025-02-10 22:10:06 -0700

    update for ready

--- ui/sections/about/AboutCard.tsx
+++ ui/sections/about/AboutCard.tsx
@@ -1,94 +1,94 @@
-"use client";
-import React, { useState } from "react";
-import Link from "next/link";
-import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
-import GitHubButton from "react-github-btn";
-function AboutCard(props: any) {
-  const data = props?.data;
-  const [like, setLike] = useState(false);
-  console.log(data)
-  const handleLike = () => {
-    setLike(true);
-  };
+// "use client";
+// import React, { useState } from "react";
+// import Link from "next/link";
+// import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
+// import GitHubButton from "react-github-btn";
+// function AboutCard(props: any) {
+//   const data = props?.data;
+//   const [like, setLike] = useState(false);
+//   console.log(data)
+//   const handleLike = () => {
+//     setLike(true);
+//   };
 
-  const handleDisLike = () => {
-    setLike(false);
-  };
+//   const handleDisLike = () => {
+//     setLike(false);
+//   };
 
-  return (
-    <div className="flex mx-auto w-fit max-w-xs ">
-      <div className="pb-3 p-0 md:p-8 sm:p-4 bg-white rounded-xl border border-gray-200  max-w-sm shadow-2xl shadow-slate-300 dark:shadow-black dark:bg-black dark:border-gray-800 mx-auto content-center justify-center w-fit ">
-        <Link href="">
-          <img
-            className="w-48 sm:w-60  lg:w-72  xl:w-96 rounded-sm gap-10 m-5 object-cover scale-100 mx-auto content-center justify-center bg-blue-900"
-            src="/twin-transparent.png"
-            alt="Collection Image"
-          />
-        </Link>
-        <div className="flex justify-center mt-3 content-center items-center text-center text-xs md:text-sm font-bold">
-          <div
-            className=" mt-0.5 text-gray-900 dark:text-white tracking-tight pr-3"
-            key={data?.followers}
-          >
-            <p className="font-extrabold">Followers</p>
-            {data && data?.followers}
-          </div>
-          <div
-            className=" mt-0.5 text-gray-900 dark:text-white tracking-tight pr-3"
-            key={data?.following}
-          >
-            <p className="font-extrabold">Following</p>
-            {data && data?.following}
-          </div>
-          <div
-            className="mt-0.5 text-gray-900 dark:text-white tracking-tight pr-3"
-            key={data?.repos}
-          >
-            <p className="font-extrabold">{data ? 'Repos' : ""}</p>
-            {data?.public_repos}
-          </div>
-        </div>
+//   return (
+//     <div className="flex mx-auto w-fit max-w-xs ">
+//       <div className="pb-3 p-0 md:p-8 sm:p-4 bg-white rounded-xl border border-gray-200  max-w-sm shadow-2xl shadow-slate-300 dark:shadow-black dark:bg-black dark:border-gray-800 mx-auto content-center justify-center w-fit ">
+//         <Link href="">
+//           <img
+//             className="w-48 sm:w-60  lg:w-72  xl:w-96 rounded-sm gap-10 m-5 object-cover scale-100 mx-auto content-center justify-center bg-blue-900"
+//             src="/twin-transparent.png"
+//             alt="Collection Image"
+//           />
+//         </Link>
+//         <div className="flex justify-center mt-3 content-center items-center text-center text-xs md:text-sm font-bold">
+//           <div
+//             className=" mt-0.5 text-gray-900 dark:text-white tracking-tight pr-3"
+//             key={data?.followers}
+//           >
+//             <p className="font-extrabold">Followers</p>
+//             {data && data?.followers}
+//           </div>
+//           <div
+//             className=" mt-0.5 text-gray-900 dark:text-white tracking-tight pr-3"
+//             key={data?.following}
+//           >
+//             <p className="font-extrabold">Following</p>
+//             {data && data?.following}
+//           </div>
+//           <div
+//             className="mt-0.5 text-gray-900 dark:text-white tracking-tight pr-3"
+//             key={data?.repos}
+//           >
+//             <p className="font-extrabold">{data ? 'Repos' : ""}</p>
+//             {data?.public_repos}
+//           </div>
+//         </div>
 
-        <div className="flex justify-between mx-5 items-center mt-2 h-12">
-          <h2
-            className="text-sm md:text-base lg:text-lg font-extrabold tracking-tight text-gray-900 dark:text-white"
-            key=""
-          >
-            <Link href="https://github.com/twinnytwin22">@twinnytwin22</Link>
-          </h2>
-          <div className="m-2">
-            {!like ? (
-              <>
-                <IoIosHeartEmpty
-                  onClick={handleLike}
-                  className=" text-2xl md:text-3xl text-black dark:text-white"
-                />
-              </>
-            ) : (
-              <>
-                <IoIosHeart
-                  onClick={handleDisLike}
-                  className="text-xl md:text-3xl"
-                  fill="red"
-                />
-              </>
-            )}
-          </div>
-          <div className="">
-            <GitHubButton
-              href="https://github.com/twinnytwin22"
-              aria-label="Follow @twinnytwin22 on GitHub"
-              data-color-scheme="red"
-              data-size="large"
-            >
-              Follow
-            </GitHubButton>
-          </div>
-        </div>
-        <div className="mx-auto" key=""></div>
-      </div>
-    </div>
-  );
-}
+//         <div className="flex justify-between mx-5 items-center mt-2 h-12">
+//           <h2
+//             className="text-sm md:text-base lg:text-lg font-extrabold tracking-tight text-gray-900 dark:text-white"
+//             key=""
+//           >
+//             <Link href="https://github.com/twinnytwin22">@twinnytwin22</Link>
+//           </h2>
+//           <div className="m-2">
+//             {!like ? (
+//               <>
+//                 <IoIosHeartEmpty
+//                   onClick={handleLike}
+//                   className=" text-2xl md:text-3xl text-black dark:text-white"
+//                 />
+//               </>
+//             ) : (
+//               <>
+//                 <IoIosHeart
+//                   onClick={handleDisLike}
+//                   className="text-xl md:text-3xl"
+//                   fill="red"
+//                 />
+//               </>
+//             )}
+//           </div>
+//           <div className="">
+//             <GitHubButton
+//               href="https://github.com/twinnytwin22"
+//               aria-label="Follow @twinnytwin22 on GitHub"
+//               data-color-scheme="red"
+//               data-size="large"
+//             >
+//               Follow
+//             </GitHubButton>
+//           </div>
+//         </div>
+//         <div className="mx-auto" key=""></div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default AboutCard;
+// export default AboutCard;
--- ui/sections/about/AboutMe.tsx
+++ ui/sections/about/AboutMe.tsx
@@ -1,145 +1,192 @@
-"use client";
-import React from "react";
-import { GradientText } from "@/ui/misc/GradientText";
-import ContactButton from "@/ui/buttons/ContactButton";
-import { ScrollDown } from "@/ui/misc/ScrollDown";
-import resume from "@/ui/sections/experience/experience.json";
-import { FaCheckCircle, FaMapPin } from "react-icons/fa";
-import LinkedInButton from "@/ui/buttons/LinkedIn";
-import { DownloadButton } from "@/ui/buttons/DownloadButton";
-import Slider from "react-slick";
-import MyGitHubButton from "@/ui/buttons/CustomGitHubButton";
-const AboutMe = () => {
+// "use client";
+// import React from "react";
+// import { GradientText } from "@/ui/misc/GradientText";
+// import ContactButton from "@/ui/buttons/ContactButton";
+// import { ScrollDown } from "@/ui/misc/ScrollDown";
+// //import resume from "@/ui/sections/experience/experience.json";
+// import { FaCheckCircle, FaMapPin } from "react-icons/fa";
+// import LinkedInButton from "@/ui/buttons/LinkedIn";
+// import { DownloadButton } from "@/ui/buttons/DownloadButton";
+// import Slider from "react-slick";
+// import MyGitHubButton from "@/ui/buttons/CustomGitHubButton";
+// const AboutMe = () => {
 
-  let settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+//   const resume = {
+//     location: "San Diego, CA",
+//     descriptions: [
+//       "I am a software engineer with a passion for creating innovative solutions to complex problems.",
+//       "I have experience in full-stack development, with a focus on building scalable and efficient web applications.",
+//       "I am always eager to learn new technologies and improve my skills.",
+//     ],
+//     education: [
+//       {
+//         field: "Computer Science",
+//         degree: "Bachelor of Science",
+//         school: "University of California, San Diego",
+//         startDate: "2018",
+//         endDate: "2022",
+//       },
+//       {
+//         field: "Software Engineering",
+//         degree: "Master of Science",
+//         school: "University of California, San Diego",
+//         startDate: "2022",
+//         endDate: "2024",
+//       },
+//     ],
+//     experience: [
+//       {
+//         title: "Software Engineer Intern",
+//         company: "Tech Company A",
+//         location: "San Francisco, CA",
+//         startDate: "June 2021",
+//         endDate: "August 2021",
+//       },
+//       {
+//         title: "Software Engineer Intern",
+//         company: "Tech Company B",
+//         location: "Los Angeles, CA",
+//         startDate: "June 2022",
+//         endDate: "August 2022",
+//       },
+//       {
+//         title: "Software Engineer Intern",
+//         company: "Tech Company C",
+//         location: "Remote",
+//         startDate: "June 2023",
+//         endDate: "August 2023",
+//       },
+//     ],
+//   };
+//   let settings = {
+//     dots: false,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//   };
 
 
-  return (
-    <> <div className="absolute top-0 left-0 w-full h-full bg-slate-100 dark:bg-black  opacity-10 bg-cover bg-[url(https://i.imgur.com/XqbXLxL.png)]" />
-      <div
-        className=" mx-auto h-full max-w-screen relative "
-        id="about"
+//   return (
+//     <> <div className="absolute top-0 left-0 w-full h-full bg-slate-100 dark:bg-black  opacity-10 bg-cover bg-[url(https://i.imgur.com/XqbXLxL.png)]" />
+//       <div
+//         className=" mx-auto h-full max-w-screen relative "
+//         id="about"
 
-      >
-        <div className="w-screen mx-auto items-center h-full ">
-          <ScrollDown />
-          <div className=" grid grid-cols-1 lg:grid-cols-12 min-h-full mx-auto max-w-screen-2xl place-items-center px-8 lg:gap-24 ">
+//       >
+//         <div className="w-screen mx-auto items-center h-full ">
+//           <ScrollDown />
+//           <div className=" grid grid-cols-1 lg:grid-cols-12 min-h-full mx-auto max-w-screen-2xl place-items-center px-8 lg:gap-24 ">
 
-            <div className="min-w-full h-full col-span-6  mx-auto ml-2 space-y-6 xl:text-8xl text-5xl sm:text-7xl lg:items-center">
-              <div className="flex text-center p-4 items-end lg:items-center min-h-full ">
-                <div className="content-start justify-start md:justify-center md:content-center items-center mx-auto">
-                  <div className="pl-5">     <GradientText text="About Me" /></div>
+//             <div className="min-w-full h-full col-span-6  mx-auto ml-2 space-y-6 xl:text-8xl text-5xl sm:text-7xl lg:items-center">
+//               <div className="flex text-center p-4 items-end lg:items-center min-h-full ">
+//                 <div className="content-start justify-start md:justify-center md:content-center items-center mx-auto">
+//                   <div className="pl-5">     <GradientText text="About Me" /></div>
 
 
-                  {[resume].map((my: any, i) => (
-                    <div key={i}>
-                      <div className="w-full text-base md:text-lg lg:text-xl p-4 sm:p-8 md:pr-8 lg:pr-16 items-center min-h-full text-left text-gray-900 dark:text-slate-200 ">
-                        <div className="flex h-10 items-center">
-                          <FaMapPin />
-                          <h1 className="text-sm  h-fit font-semibold text-left">
-                            {my.location}
-                          </h1>
-                        </div>
-                        <div className="overflow-auto h-56 lg:h-full mx-auto p-4 bg-slate-200 dark:bg-black rounded-lg  shadow-lg  shadow-slate-300 dark:shadow-gray-900">
+//                   {[resume].map((my: any, i) => (
+//                     <div key={i}>
+//                       <div className="w-full text-base md:text-lg lg:text-xl p-4 sm:p-8 md:pr-8 lg:pr-16 items-center min-h-full text-left text-gray-900 dark:text-slate-200 ">
+//                         <div className="flex h-10 items-center">
+//                           <FaMapPin />
+//                           <h1 className="text-sm  h-fit font-semibold text-left">
+//                             {my.location}
+//                           </h1>
+//                         </div>
+//                         <div className="overflow-auto h-56 lg:h-full mx-auto p-4 bg-slate-200 dark:bg-black rounded-lg  shadow-lg  shadow-slate-300 dark:shadow-gray-900">
 
-                          {my.descriptions.map((item: any, i: any) => (
-                            <div className=" lg:text-base text-sm text-ellipsis flex border-b border-slate-400 p-2 text-slate-950 dark:text-white dark:border-slate-900 items-center -ml-3" key={i}>
-                              <div className="p-3"><FaCheckCircle /></div>
-                              <p> {item}</p>
-                            </div>))}
-                        </div>
-                      </div>
+//                           {my.descriptions.map((item: any, i: any) => (
+//                             <div className=" lg:text-base text-sm text-ellipsis flex border-b border-slate-400 p-2 text-slate-950 dark:text-white dark:border-slate-900 items-center -ml-3" key={i}>
+//                               <div className="p-3"><FaCheckCircle /></div>
+//                               <p> {item}</p>
+//                             </div>))}
+//                         </div>
+//                       </div>
 
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="block p-0 col-span-6 lg:items-center w-full px-8 lg:max-w-2xl items-start fmx-auto content-center justify-center ml-2 lg:mt-8 lg:px-4 xl:mr-16 lg:mr-12">
-              <Slider
-                {...settings}
-                className="mb-8 p-8 sm:p-12 h-fit rounded-lg bg-slate-200 dark:bg-black shadow-lg mx-auto shadow-slate-300 dark:shadow-gray-900 "
-              >
-                <>
-                  <h1 className="text-xl md:text-3xl">Education</h1>
-                  <div className="md:grid grid-cols-2 ">
-                    {resume.education.map((my: any, i: any) => (
-                      <div className="block p-2.5 lg:text-base text-sm" key={i}>
-                        <p className="font-semibold">{my.field}</p>
-                        <p className="font-semibold italic">
-                          {my.degree}
-                        </p>
-                        <div className="flex italic">
-                          <p className="">{my.school}</p>,&nbsp;
-                          <p className="">{my.startDate}</p>-
-                          <p className="">{my.endDate}</p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </>
-                <>
-                  <div className="">
-                    <h1 className=" text-xl md:text-3xl">Experience</h1>
-                    <div className="md:grid grid-cols-2 ">
+//                     </div>
+//                   ))}
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="block p-0 col-span-6 lg:items-center w-full px-8 lg:max-w-2xl items-start fmx-auto content-center justify-center ml-2 lg:mt-8 lg:px-4 xl:mr-16 lg:mr-12">
+//               <Slider
+//                 {...settings}
+//                 className="mb-8 p-8 sm:p-12 h-fit rounded-lg bg-slate-200 dark:bg-black shadow-lg mx-auto shadow-slate-300 dark:shadow-gray-900 "
+//               >
+//                 <>
+//                   <h1 className="text-xl md:text-3xl">Education</h1>
+//                   <div className="md:grid grid-cols-2 ">
+//                     {resume.education.map((my: any, i: any) => (
+//                       <div className="block p-2.5 lg:text-base text-sm" key={i}>
+//                         <p className="font-semibold">{my.field}</p>
+//                         <p className="font-semibold italic">
+//                           {my.degree}
+//                         </p>
+//                         <div className="flex italic">
+//                           <p className="">{my.school}</p>,&nbsp;
+//                           <p className="">{my.startDate}</p>-
+//                           <p className="">{my.endDate}</p>
+//                         </div>
+//                       </div>
+//                     ))}
+//                   </div>
+//                 </>
+//                 <>
+//                   <div className="">
+//                     <h1 className=" text-xl md:text-3xl">Experience</h1>
+//                     <div className="md:grid grid-cols-2 ">
 
-                      {resume.experience.slice(0, 2).map((job: any, i: any) => (
-                        <div key={i} className="p-1.5">
-                          <p className="text-base font-semibold">{job.title}</p>
-                          <div className="flex">
-                            <p className="text-base">{job.company}</p>&nbsp;-&nbsp;
-                            <p className="text-base">{job.location}</p>
-                          </div>
-                          <div className="flex italic">
-                            <p className="text-base">{job.startDate}</p>
-                            &nbsp;-&nbsp;
-                            <p className="text-base">{job.endDate}</p>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </>
-                <>
-                  <div className="">
-                    <h1 className=" text-xl md:text-3xl">Experience</h1>
-                    <div className="md:grid grid-cols-2 ">
+//                       {resume.experience.slice(0, 2).map((job: any, i: any) => (
+//                         <div key={i} className="p-1.5">
+//                           <p className="text-base font-semibold">{job.title}</p>
+//                           <div className="flex">
+//                             <p className="text-base">{job.company}</p>&nbsp;-&nbsp;
+//                             <p className="text-base">{job.location}</p>
+//                           </div>
+//                           <div className="flex italic">
+//                             <p className="text-base">{job.startDate}</p>
+//                             &nbsp;-&nbsp;
+//                             <p className="text-base">{job.endDate}</p>
+//                           </div>
+//                         </div>
+//                       ))}
+//                     </div>
+//                   </div>
+//                 </>
+//                 <>
+//                   <div className="">
+//                     <h1 className=" text-xl md:text-3xl">Experience</h1>
+//                     <div className="md:grid grid-cols-2 ">
 
-                      {resume.experience.slice(2, 4).map((job: any, i: any) => (
-                        <div key={i} className="p-1.5">
-                          <p className="text-base font-semibold">{job.title}</p>
-                          <div className="flex">
-                            <p className="text-base">{job.company}</p>&nbsp;-&nbsp;
-                            <p className="text-base">{job.location}</p>
-                          </div>
-                          <div className="flex italic">
-                            <p className="text-base">{job.startDate}</p>
-                            &nbsp;-&nbsp;
-                            <p className="text-base">{job.endDate}</p>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </>
-              </Slider>
-              <div className="hidden md:flex h-fit space-x-2">
-                <ContactButton color="text-blue-800 dark:text-blue-600" />
-                <LinkedInButton />
-                <MyGitHubButton />
-                <DownloadButton />
-              </div>
-            </div>
-          </div>
-        </div></div>
-    </>
-  );
-};
+//                       {resume.experience.slice(2, 4).map((job: any, i: any) => (
+//                         <div key={i} className="p-1.5">
+//                           <p className="text-base font-semibold">{job.title}</p>
+//                           <div className="flex">
+//                             <p className="text-base">{job.company}</p>&nbsp;-&nbsp;
+//                             <p className="text-base">{job.location}</p>
+//                           </div>
+//                           <div className="flex italic">
+//                             <p className="text-base">{job.startDate}</p>
+//                             &nbsp;-&nbsp;
+//                             <p className="text-base">{job.endDate}</p>
+//                           </div>
+//                         </div>
+//                       ))}
+//                     </div>
+//                   </div>
+//                 </>
+//               </Slider>
+//               <div className="hidden md:flex h-fit space-x-2">
+//                 <ContactButton color="text-blue-800 dark:text-blue-600" />
+//                 <LinkedInButton />
+//                 <MyGitHubButton />
+//                 <DownloadButton />
+//               </div>
+//             </div>
+//           </div>
+//         </div></div>
+//     </>
+//   );
+// };
 
-export default AboutMe;
+// export default AboutMe;
